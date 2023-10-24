953ab9d19e47deb9b7795ae19b290a70134fc62f
Walter Vásquez <waltervasquez.2002@gmail.com>
2023-10-24 10:27:33 -0300

    Editando leer.cpp

--- leer.cpp
+++ leer.cpp
@@ -3,6 +3,30 @@
 
 FILE archivo;
 
-FILE *fopen(const char *archivo, const char *wr){
+void crearArchivo(const char* nombreArchivo) {
+    // Intentar abrir el archivo en modo lectura
+    FILE* archivo = fopen(nombreArchivo, "r");
+    
+    // Si el archivo no existe, fopen devolverá NULL
+    if (archivo == NULL) {
+        // El archivo no existe, así que lo creamos
+        archivo = fopen(nombreArchivo, "w");
+        
+        if (archivo != NULL) {
+            printf("Archivo '%s' creado con éxito.\n", nombreArchivo);
+            fclose(archivo); // Cerrar el archivo después de crearlo
+        } else {
+            printf("No se pudo crear el archivo '%s'.\n", nombreArchivo);
+        }
+    } else {
+        // El archivo ya existe, así que no hacemos nada
+        printf("El archivo '%s' ya existe. No se creó uno nuevo.\n", nombreArchivo);
+        fclose(archivo); // Cerrar el archivo que se abrió en modo lectura
+    }
+}
 
-};
+int main() {
+    const char* nombreArchivo = "miarchivo.txt";
+    crearArchivo(nombreArchivo);
+    return 0;
+}
