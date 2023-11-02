901b1e1f5f9a454f037f66234b9ddefdf2d91aee
JosefaPinilla <josefa.pinilla01@gmail.com>
2023-11-02 14:25:41 -0300

    arreglo funcion modificar

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 
 int ObtenerContador() {
 
+    // Instanciamos un nuevo archivo llamado contador.txt
     FILE *contadorArchivo = fopen("contador.txt", "r");
     if (contadorArchivo == NULL) {
+        return 0; // Si el archivo no existe, el contador se inicia en 0
     }
     int contador;
 
@@ -12,6 +14,7 @@ int ObtenerContador() {
     return contador;
 }
 
+// Declarar una función para actualizar el contador
 void ActualizarContador(int nuevoContador) {
     FILE *contadorArchivo = fopen("contador.txt", "w");
     if (contadorArchivo == NULL) {
@@ -23,6 +26,7 @@ void ActualizarContador(int nuevoContador) {
 }
 
 
+// Declarar la función Ingresar
 void Ingresar() {
     FILE *archivo = fopen("archivo.dat", "a");
     if (archivo == NULL) {
@@ -30,14 +34,18 @@ void Ingresar() {
         return;
     }
     
+    // Obtener el valor actual del contador
     int numeroFila = ObtenerContador();
 
+    // Incrementar el contador para la siguiente inserción
     numeroFila++;
 
+    // Declarar variables para almacenar los datos del auto
     char marca[100], modelo[100], tipo_gasolina[100], color[100];
     float motor_cilindrada;
     int cantidad_asientos, cantidad_puertas;
     
+    // Solicitar al usuario que ingrese los datos del auto
     printf("Marca: ");
     scanf("%s", marca);
     printf("Modelo: ");
@@ -53,16 +61,21 @@ void Ingresar() {
     printf("Color: ");
     scanf("%s", color);
 
+    // Escribir los datos del auto en el archivo en un formato específico con el número de fila
     fprintf(archivo, "%d: %s-%s-%.1f-%s-%d-%d-%s\n", numeroFila, marca, modelo, motor_cilindrada, tipo_gasolina, cantidad_asientos, cantidad_puertas, color);
-    printf("Auto ingresado con exito.\n");
+    printf("Auto ingresado con éxito.\n");
 
+    // Actualizar el contador en el archivo
     ActualizarContador(numeroFila);
 
+    // Cerrar el archivo
     fclose(archivo);
 }
 
 
+// Función para mostrar los datos de todos los autos en el archivo
 void Mostrar() {
+    // Abrir el archivo en modo lectura
     FILE *archivo = fopen("archivo.dat", "r");
     if (archivo == NULL) {
         printf("No se pudo abrir el archivo.\n");
@@ -70,14 +83,15 @@ void Mostrar() {
     }
 
     char linea[1000];
+    // Leer y mostrar cada línea del archivo
     while (fgets(linea, sizeof(linea), archivo) != NULL) {
         printf("%s \n", linea);
     }
 
+    // Cerrar el archivo
     fclose(archivo);
 }
 
-<<<<<<< HEAD
 // Función para eliminar todos los autos del archivo
 void Eliminar() {
     // Abrir el archivo original en modo lectura
@@ -197,11 +211,10 @@ void Modificar() {
         return;
     }
 }
-=======
->>>>>>> e2ee8b2170e2d340c7a5c59ba3748d32e6d1a15c
 
-
+// Función para cambiar el formato de separación en el archivo
 void CambiarFormatoEnArchivo() {
+    // Abrir el archivo en modo lectura y escritura
     FILE *archivo = fopen("archivo.dat", "r+");
     if (archivo == NULL) {
         printf("No se pudo abrir el archivo.\n");
@@ -209,69 +222,23 @@ void CambiarFormatoEnArchivo() {
     }
 
     char formato;
-    printf("Ingresa el nuevo separador ('-' o '/' o ';' o ':'): ");
+    // Solicitar al usuario que ingrese el nuevo caracter de separación
+    printf("Ingresa el caracter para reemplazar ('-' o '/'): ");
     scanf(" %c", &formato);
 
     int caracter;
+    // Leer cada caracter en el archivo y reemplazar '-' o '/' por el nuevo caracter
     while ((caracter = fgetc(archivo)) != EOF) {
-        if (caracter == '-' || caracter == '/' || caracter == ';' || caracter == ':') {
+        if (caracter == '-' || caracter == '/') {
             fseek(archivo, -1, SEEK_CUR);
             fputc(formato, archivo);
             fseek(archivo, 0, SEEK_CUR);
         }
     }
 
+    // Cerrar el archivo
     fclose(archivo);
-    printf("Cambio de separador realizado correctamente.\n");
-}
-
-void Eliminar() {
-    FILE *archivoLectura = fopen("archivo.dat", "r");
-    if (archivoLectura == NULL) {
-        printf("No se pudo abrir el archivo para lectura.\n");
-        return;
-    }
-
-    // Abrir un archivo temporal en modo escritura
-    FILE *archivoTemporal = fopen("temporal.dat", "w");
-    if (archivoTemporal == NULL) {
-        printf("No se pudo abrir el archivo temporal.\n");
-        fclose(archivoLectura);
-        return;
-    }
-
-    int idAEliminar;
-    printf("Ingrese el ID del automóvil que desea eliminar: ");
-    scanf("%d", &idAEliminar);
-
-    int idAutomovil;
-    char linea[1000]; // Suponemos que cada línea en el archivo tiene un máximo de 1000 caracteres
-
-    while (fgets(linea, sizeof(linea), archivoLectura) != NULL) {
-        sscanf(linea, "%d", &idAutomovil);
-
-        if (idAutomovil == idAEliminar) {
-            printf("El automóvil con el ID %d ha sido eliminado.\n", idAEliminar);
-        } else {
-            fprintf(archivoTemporal, "%s", linea);
-        }
-    }
-
-    // Cerrar los archivos
-    fclose(archivoLectura);
-    fclose(archivoTemporal);
-
-    // Eliminar el archivo original
-    if (remove("archivo.dat") != 0) {
-        printf("Error al eliminar el archivo original.\n");
-        return;
-    }
-
-    // Renombrar el archivo temporal
-    if (rename("temporal.dat", "archivo.dat") != 0) {
-        printf("Error al renombrar el archivo temporal.\n");
-        return;
-    }
+    printf("Reemplazo completado.\n");
 }
 
 int main() {
@@ -279,21 +246,12 @@ int main() {
     do {
         printf("-------------------- CRUD DE AUTOMOVILES --------------------");
         printf("\nMenu de opciones:\n");
-<<<<<<< HEAD
         printf("1. Ingresar\n");
         printf("2. Mostrar\n");
         printf("3. Eliminar\n");
-        printf("4. Modificar \n");
+        printf("4. Modificar\n");
         printf("5. Cambiar el formato en el archivo.dat\n");
         printf("0. Salir\n");
-=======
-        printf("1.- Ingresar\n");
-        printf("2.- Mostrar\n");
-        printf("3.- Eliminar\n");
-        printf("4.- Cambiar el separador\n");
-        printf("5.- Modificar (No disponible)\n");
-        printf("0.- Salir\n");
->>>>>>> e2ee8b2170e2d340c7a5c59ba3748d32e6d1a15c
         printf("-------------------- CRUD DE AUTOMOVILES -------------------- \n");
         printf("Ingrese su eleccion: ");
         scanf("%d", &opcion);
